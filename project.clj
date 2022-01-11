37b892692b31b9d287ff94036d55558f0411fee7
Michiel Borkent <michielborkent@gmail.com>
2022-01-11 22:28:06 +0100

    upgrade etaoin-graal

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :source-paths ["src"]
   :resource-paths ["resources"]
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [borkdude/etaoin-graal "0.4.2-deb92aaa21c59c52521f42c0177bbce9ac10a0e2"]
+                 [borkdude/etaoin-graal "0.4.6-ae21d07"]
                  [nrepl/bencode "1.1.0"]]
   :profiles {:uberjar {:global-vars {*assert* false}
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"
